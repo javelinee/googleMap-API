9ced2d1a1f407c1071a35a087e38b92b6424a9cc
javelinee <javeline27@gmail.com>
2020-05-22 16:44:03 +0700

    api google

---
+++ app/src/main/java/com/project/googlemapapi/MainActivity.java
@@ -0,0 +1,34 @@
+package com.project.googlemapapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng loc = new LatLng(-6.180643, 106.798140);
+        map.addMarker(new MarkerOptions().position(loc).title("Kost"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 16));
+
+    }
+}
